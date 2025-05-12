5a3a8248949948b7f3805f253465a249514a2eef
GalacticBurrito <157920255+Galacticburrito@users.noreply.github.com>
2025-05-12 12:08:05 -0400

    Create durability.rs

---
+++ src/durability.rs
@@ -0,0 +1,28 @@
+use crate::SystemUpdateSet;
+use bevy::prelude::*;
+
+pub struct DurabilityPlugin {}
+
+impl Plugin for DurabilityPlugin {
+    fn build(&self, app: &mut App) {
+      app
+            .register_type::<Durability>();
+    }
+}
+
+#[derive(Component, Reflect)]
+pub struct Durability {
+    max: f32
+    current: f32
+    decay_rate: f32,
+}
+
+impl Durability {
+    pub fn new(max: f32, decay_rate: f32) -> Self {
+        Durability {
+            max,
+            current: max,
+            decay_rate,
+        }
+    }
+}
